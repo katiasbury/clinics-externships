b8a55515ab6181fd2c7529930209f98c3b5bbd38
Kati Asbury <asburykati@gmail.com>
2018-05-30 22:25:37 -0400

    Added class Supervisor which extends class User

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 // Includes
 require 'classes/user.php';
+require 'classes/supervisor.php';
 
 // Initialize variables
 $user = new User("kasbury");
+$supervisor = new Supervisor(0);
 
 // Return user role
-echo "kasbury = " . $user->getRole();
+print_r($supervisor->getPrograms());
 
  ?>
